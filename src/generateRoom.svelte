b5a8e89ec6ec606b10f7d3df76a98b4bcf757320
Omar-ALkhateeb <omar.alkhateeb9919@gmail.com>
2021-09-03 23:53:44 +0300

    updated main page

--- src/generateRoom.svelte
+++ src/generateRoom.svelte
@@ -17,6 +17,21 @@
   }
 </script>
 
+<h2>app info</h2>
+<ul>
+  <li>
+    <span>*</span> uses decentralized database so all your data is safe from our
+    lizard overloads
+  </li>
+  <li>
+    <span>*</span>
+    data is persisted but not stored in any server and cant be accesed by us
+  </li>
+  <li>
+    <span>*</span>
+    data depends on users being active so might not be avaiblabne 24/7 or forever
+  </li>
+</ul>
 <input bind:value={generatedUrl} />
 <button class="login" on:click={generate}>generate room</button>
 {#if generatedUrl}
@@ -24,3 +39,19 @@
     share this link with a friend or more and visit it to start chatting!
   </h3>
 {/if}
+
+<style>
+  * {
+    color: white;
+  }
+  ul {
+    margin-bottom: 40px;
+  }
+  span {
+    color: tomato;
+  }
+  h2 {
+    margin-left: 36px;
+    text-align: start;
+  }
+</style>
