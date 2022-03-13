b8642b9cd46f0b706d45d4a852c68d193f7d63df
s-net <95741151+s0net@users.noreply.github.com>
2022-03-13 15:32:29 +0530

    add title to see the name of sender

--- src/ChatMessage.svelte
+++ src/ChatMessage.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div class={`message ${messageClass}`}>
-  <img src={avatar} alt="avatar" />
+  <img src={avatar} alt="avatar" title={message.who}/>
   <div class="message-text">
     <p>{message.what}</p>
 
